20fbf75db992cffe0859c48cb483ac0c6a32f584
Jared Harris <jared.harris1@g.austincc.edu>
2018-12-22 02:09:48 -0600

    cant get click working

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -11,16 +11,39 @@ class Container extends Component {
         topScore: 0
     };
 
-    selectKanji = (id) => {
-        
+    selectKanji = (id, clicked) => {
+      if(!this.state.clicked.includes(id)){
+        return this.setState({
+          image: this.state.images.sort(() => Math.random() - 0.5),
+          // message: "Not so lucky this time. Give it another shot",
+          score: this.state.score + 1,
+          clicked: [...this.state.clicked, id]
+        })}
+        else {
+        alert(" You lose")
       }
+    }
   
-      
+
       render() {
         return(
+        <div  className="container-fluid">
+          <p> 
+            Think you can Remember The Kanji? Click around, and be careful not to click an image twice
+          <br />
+          Current Score: {this.state.score} | Top Score: {this.state.topScore}
+          </p>
           <div>
-      
+            {
+              this.state.images.map(image => 
+                <Card
+                  key={image.id}
+                  image={image.imageLink}
+                />
+              )
+            }
           </div>
+        </div>
         )
       }
 
